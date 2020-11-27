ec51ef4f4f4a8e37c95cc86f4bb764247a293256
Henrik Rydgård <hrydgard@gmail.com>
2020-11-27 23:58:56 +0100

    Minor cleanup

--- Common/Thread/Executor.cpp
+++ Common/Thread/Executor.cpp
@@ -5,10 +5,6 @@
 
 namespace threading {
 
-void SameThreadExecutor::Run(std::function<void()> func) {
-	func();
-}
-
 void NewThreadExecutor::Run(std::function<void()> func) {
 	threads_.push_back(std::thread(func));
 }
